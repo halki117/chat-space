d18acdc187bfca100286cf9ff637c0c670e79e16
halki <s.halki0203@icloud.com>
2020-01-17 20:15:10 +0900

    chatspase_automaticupdate03

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -62,7 +62,6 @@ $(function(){
   
   $('#new_message').on('submit', function(e){
   e.preventDefault();
-  console.log("aaa")
   var formData = new FormData(this);
   var url = $(this).attr('action')
 
@@ -116,7 +115,7 @@ $(function(){
       }
     })
     .fail(function() {
-      console.log('error');
+      alert('error');
     });
   };
   
